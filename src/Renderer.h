77e8fee842d95e7b881670b8594da927cdf8f103
wootguy <w00tguy123@gmail.com>
2025-06-22 01:08:04 -0700

    fit model to window

--- src/Renderer.h
+++ src/Renderer.h
@@ -1,6 +1,7 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 #include "ShaderProgram.h"
+#include "colors.h"
 
 class Model;
 class MdlRenderer;
@@ -19,6 +20,7 @@ private:
 
 	GLFWwindow* window;
 	ShaderProgram* mdlShader = NULL;
+	ShaderProgram* colorShader = NULL;
 	int windowWidth;
 	int windowHeight;
 	float fov = 75.0f;
@@ -33,4 +35,6 @@ private:
 
 	bool create_window();
 	void compile_shaders();
+	float get_model_fit_distance(vec3 modelOrigin, vec3 modelAngles); // get distance needed to fit the model to the rendering window
+	void drawBoxOutline(vec3 center, vec3 mins, vec3 maxs, COLOR4 color);
 };
